c1dbc738550620efb4ad248660952b69e608ea0f
pwiatr3 <pwiatr3@yahoo.pl>
2022-06-22 09:07:46 +0200

    17. Creating a 2D array with basic operations done!

--- problem_17/main.cpp
+++ problem_17/main.cpp
@@ -2,21 +2,28 @@
 // 17. Creating a 2D array with basic operations
 //******************************************************
 #include <iostream>
-#include  <exception>
+#include <exception>
 #include <vector>
-
+#include <algorithm>
+#include <iterator>
 using namespace std;
 template<class T, size_t R, size_t C>
 class array2d {
     
 public:
+    typedef T* iterator;
+    typedef  const T* const_iterator;
+
     T arr[R][C] {0};
     array2d() {}
+    array2d(array2d&& other) {
+        move(other.begin(), other.end(), begin());
+    }
     constexpr array2d(std::initializer_list<T> init_list) {
         if((R * C) != init_list.size()) {
             throw length_error("Initilalizer list has wrong size");
         }
-        auto it = begin(init_list);
+        auto it = init_list.begin();
         for( int row = 0; row < R; row++) {
             for(int col = 0; col < C; col++) {
                 arr[row][col] = *it++;
@@ -38,6 +45,7 @@ public:
     constexpr const T* data() const {
         return &arr[0][0];
     }
+
     constexpr T& at(size_t row, size_t col) {
         if(row >= 0 && row < R && col >= 0 && col < C) {
             return arr[row][col];
@@ -49,6 +57,9 @@ public:
             return arr[row][col];
         }
         throw out_of_range("Array index is out of range!");
+    }
+    T& operator()(size_t i, size_t j) {
+        return arr[i][j];
     }
     constexpr void print() const {
         for(int row = 0; row < R; row++) {
@@ -77,33 +88,55 @@ public:
             }
         }
     }
+    
+    iterator begin() { return data(); }  
+    iterator end() { return data() + size(1) * size(2); }
+    const_iterator begin() const { return data(); }
+    const_iterator end() const { return data() + size(1) * size(2); }
 };
 
 int main() {
-    array2d<int, 2, 3> arr {1, 2, 3, 4, 5, 6};
-    arr.print();
-    arr.at(0,0) = 4;
-    arr.print();
-    int* ptr = arr.data();
-    ptr[0] = 1;
-    // for(int i = 0; i < arr.size(1) * arr.size(2); i++) {
-    //     cout << ptr[i] << ' ';
-    // }
-    // cout << endl;
-    arr.print();
-    arr.fill(1);
-    arr.print();
+    //******************* MINE *************************
+    // array2d<int, 2, 3> arr {1, 2, 3, 4, 5, 6};
+    // arr.print();
+    // arr.at(0,0) = 4;
+    // arr.print();
+    // int* ptr = arr.data();
+    // ptr[0] = 1;
+    // // for(int i = 0; i < arr.size(1) * arr.size(2); i++) {
+    // //     cout << ptr[i] << ' ';
+    // // }
+    // // cout << endl;
+    // arr.print();
+    // arr.fill(1);
+    // arr.print();
 
 
-    // const array2d<int, 1, 3> arr1 {0, 1, 2};
-    // arr1.print();
-    // cout << arr1.at(0, 2);
-    // const int* ptr1 = arr1.data();
+    // // const array2d<int, 1, 3> arr1 {0, 1, 2};
+    // // arr1.print();
+    // // cout << arr1.at(0, 2);
+    // // const int* ptr1 = arr1.data();
 
-    array2d<int, 2, 3> arr2 {0, 1, 2, 3, 4, 5};
-    arr.swap(arr2);
-    arr.print();
-    arr2.print();
+    // array2d<int, 2, 3> arr2 {0, 1, 2, 3, 4, 5};
+    // arr.swap(arr2);
+    // arr.print();
+    // arr2.print();
+    
+    //******************* AUTHOR *************************
+    // dostęp do elementu
+    array2d<int, 2, 3> a {1, 2, 3, 4, 5, 6};
+    for (size_t i = 0; i < a.size(1); ++i)
+        for (size_t j = 0; j < a.size(2); ++j)
+            a(i, j) *= 2;
+    // przetwarzanie elementów
+    copy(begin(a), end(a), std::ostream_iterator<int>(std::cout, " "));
+    // wypełnianie elementów
+    array2d<int, 2, 3> b;
+    b.fill(1);
+    // zamiana elementów
+    a.swap(b);
+    // przenoszenie elementów
+    array2d<int, 2, 3> c(std::move(b));
     return 0;
 }
 
